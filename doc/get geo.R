d461e6f2e2e9c67fe0ddc43fa5c4539b57d9d724
Wanting-Cui <35549396+Wanting-Cui@users.noreply.github.com>
2018-02-13 11:05:31 -0500

    Update res loc cwt

--- doc/get geo.R
+++ doc/get geo.R
@@ -8,5 +8,5 @@ loc1 <- data %>%
     mutate(ADDRESS = paste(BUILDING, STREET, "New York, NY", sep = ", ")) %>%
     mutate_geocode(ADDRESS)
 
-path1 <- paste0("../output/Resturant/resturant_loc", name, ".csv")
+path1 <- paste0("../output/Resturant/resturant_loc_", name, ".csv")
 write.csv(loc1, file = path1)
