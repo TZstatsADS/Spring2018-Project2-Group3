68caa71690d58f03f9d930899c4038d800b1b52c
ecolalala <35772736+ecolalala@users.noreply.github.com>
2018-02-18 21:51:21 -0500

    ccc

--- app/server.R
+++ app/server.R
@@ -16,7 +16,7 @@ shinyServer(function(input, output,session) {
         output$map2 <- renderLeaflet({
                 leaflet()%>%
                         setView(lng = -73.98928, lat = 40.75042, zoom = 13)%>%
-                        addProviderTiles("Stamen.TonerHybrid")
+                        addProviderTiles("Stamen.TonerLite")
         })
         
         
@@ -26,7 +26,7 @@ shinyServer(function(input, output,session) {
         
         output$map3 <- renderLeaflet({
           leaflet()%>%
-            setView(lng = -73.98928, lat = 40.75042, zoom = 13)%>%
+            setView(lng = -74.015, lat = 40.75042, zoom = 13)%>%
             addProviderTiles("Stamen.TonerLite")
         })
         
@@ -141,6 +141,71 @@ shinyServer(function(input, output,session) {
                 }
         })
 
+        
+        
+        
+        
+        #########recommadation2
+        # observe({
+        #   cities <- if (is.null(input$states)) character(0) else {
+        #     filter(cleantable, State %in% input$states) %>%
+        #       `$`('City') %>%
+        #       unique() %>%
+        #       sort()
+        #   }
+        #   stillSelected <- isolate(input$cities[input$cities %in% cities])
+        #   updateSelectInput(session, "cities", choices = cities,
+        #                     selected = stillSelected)
+        # })
+        # 
+        # observe({
+        #   zipcodes <- if (is.null(input$states)) character(0) else {
+        #     cleantable %>%
+        #       filter(State %in% input$states,
+        #              is.null(input$cities) | City %in% input$cities) %>%
+        #       `$`('Zipcode') %>%
+        #       unique() %>%
+        #       sort()
+        #   }
+        #   stillSelected <- isolate(input$zipcodes[input$zipcodes %in% zipcodes])
+        #   updateSelectInput(session, "zipcodes", choices = zipcodes,
+        #                     selected = stillSelected)
+        # })
+        # 
+        # observe({
+        #   if (is.null(input$goto))
+        #     return()
+        #   isolate({
+        #     map <- leafletProxy("map")
+        #     map %>% clearPopups()
+        #     dist <- 0.5
+        #     zip <- input$goto$zip
+        #     lat <- input$goto$lat
+        #     lng <- input$goto$lng
+        #     showZipcodePopup(zip, lat, lng)
+        #     map %>% fitBounds(lng - dist, lat - dist, lng + dist, lat + dist)
+        #   })
+        # })
+        # 
+        # output$ziptable <- DT::renderDataTable({
+        #   df <- cleantable %>%
+        #     filter(
+        #       Score >= input$minScore,
+        #       Score <= input$maxScore,
+        #       is.null(input$states) | State %in% input$states,
+        #       is.null(input$cities) | City %in% input$cities,
+        #       is.null(input$zipcodes) | Zipcode %in% input$zipcodes
+        #     ) %>%
+        #     mutate(Action = paste('<a class="go-map" href="" data-lat="', Lat, '" data-long="', Long, '" data-zip="', Zipcode, '"><i class="fa fa-crosshairs"></i></a>', sep=""))
+        #   action <- DT::dataTableAjax(session, df)
+        #   
+        #   DT::datatable(df, options = list(ajax = list(url = action)), escape = FALSE)
+        # })
+        # 
+        # 
+        # 
+        # 
+        
  
 
 })
--- app/ui.r
+++ app/ui.r
@@ -6,7 +6,7 @@ shinyUI(
         fluidPage(
                 navbarPage("Where to rent", theme = "black.css",
                            #tabPanel("Introduction"),
-                           navbarMenu(title="Begin",
+                           #navbarMenu(title="Begin",
                                       tabPanel("Summary",
                                     div(class="outer",
                                         tags$style(type = "text/css", ".outer {position: fixed; top: 41px; left: 0; right: 0; bottom: 0; overflow: hidden; padding: 0}"),
@@ -19,7 +19,7 @@ shinyUI(
                                                       div(style="text-align:left;
                                                           box-shadow: 10px 30px 30px  #888888;
                                                           width:200px;
-                                                          height:230px;
+                                                          height:270px;
                                                           position:relative;
                                                           font-style: italic",
                                                           h6(textOutput("zip_text"),align="left"),
@@ -66,21 +66,49 @@ shinyUI(
                                         leafletOutput("map3", width = "100%", height = "100%"),
                                         absolutePanel(id = "controls", class = "panel panel-default",
                                                       fixed = TRUE, draggable = FALSE,
-                                                      top = 110, right = 10, height = "auto",width = 200,
-                                                      h4("Choose What"),
-                                                      h4(" You Want"),
-                                                      hr(),
-                                                      div(checkboxGroupInput("check_rest",label="Restaurant"),
-                                                          checkboxInput("check_tran",label="Transportation"),
-                                                          checkboxInput("check_cb",label="CLubs/Bars"),
-                                                          checkboxInput("check_ct",label="Cinema/Threate"),
-                                                          checkboxInput("check_m",label="Market")
+                                                      top = 110, left = 5, height = "auto",width = "300",
+                                                      h4("Choose What You Want"),
+                                                      div(checkboxGroupInput("check_ty","Apt Type:",
+                                                                             choiceNames=list("Studio","1B","2B", "3B","4B"),
+                                                                             choiceValues=list("tys","ty1","ty2","ty3","ty4"),inline= TRUE),
+                                                          sliderInput("apt_price", "Price Per Room:",
+                                                                        min = 0, max = 1000, value = 500),
+                                                          checkboxGroupInput("check_re","Restaurant:",
+                                                                             choiceNames=list("Chinese","American","Italian", "Japanese","Pizza", "Other"),
+                                                                             choiceValues=list("rec","rea","rei","rej","rep","reo"),inline= TRUE),
+                                                          checkboxGroupInput("check_tr","Transportation:",
+                                                                             choiceNames=list("It's everything","Emmm","Who Cares"),
+                                                                             choiceValues=list("tr1","tr2","tr3"),inline= TRUE),
+                                                          checkboxGroupInput("check_cb","Club/Bar:",
+                                                                             choiceNames=list("Let's party!","Emmm","Who Cares"),
+                                                                             choiceValues=list("cb1","cb2","cb3"),inline= TRUE),
+                                                          checkboxGroupInput("check_ct","Cinema/Theater:",
+                                                                             choiceNames=list("1","2","3"),
+                                                                             choiceValues=list("ct1","ct2","ct3"),inline= TRUE),
+                                                          checkboxGroupInput("check_ma","Market:",
+                                                                             choiceNames=list("1","2","3"),
+                                                                             choiceValues=list("ma1","ma2","ma3"),inline= TRUE)
                                                       )
                                                       
                                         )
                                     )
+                           ),
+                           tabPanel("Recommendation2",
+                                    fluidRow(
+                                      column(3,
+                                             selectInput("apt_type", "Apartment Type:",list("Studio","1B","2B", "3B","4B"), multiple=TRUE))
+                                      #column(4,
+                                             #sliderInput("apt_price", "Price Per Room:",min = 0, max = 1000, value = 500))
+                                            # conditionalPanel("input.type",
+                                           
+                                    )
+                                    ),
+                           
+                           tabPanel("Contact"
+                           
+                                    
+                                    
+                                    )
                            )
-                           )
-                )
                 )
         )
